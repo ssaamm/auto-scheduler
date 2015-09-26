1b50966983fd29ce76bb7c88fc7c6a3ecc08429e
Samuel Taylor <github@samueltaylor.org>
2015-09-26 16:37:23 -0500

    Add context.io secret handling

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -5,6 +5,7 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template
 from FlaskWebProject import app
+from os import environ
 
 @app.route('/')
 @app.route('/home')
@@ -35,3 +36,7 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+def context_keys():
+    return dict(key=environ.get('CONTEXTIO_KEY'),
+            secret=environ.get('CONTEXTIO_SECRET'))
