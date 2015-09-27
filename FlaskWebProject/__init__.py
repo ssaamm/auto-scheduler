c9c027a0b2acf26280b8abc3ac6de6f1acf25b66
Evan-Green <evan@Evans-MacBook-Pro.local>
2015-09-26 21:48:02 -0500

    Added Flask User Templates

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -16,13 +16,8 @@ class ConfigClass(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',     'sqlite:///basic_app.sqlite')
     CSRF_ENABLED = True
 
-    # Flask-Mail settings
-    MAIL_USERNAME =           os.getenv('MAIL_USERNAME',        'email@example.com')
-    MAIL_PASSWORD =           os.getenv('MAIL_PASSWORD',        'password')
-    MAIL_DEFAULT_SENDER =     os.getenv('MAIL_DEFAULT_SENDER',  '"MyApp" <noreply@example.com>')
-    MAIL_SERVER =             os.getenv('MAIL_SERVER',          'smtp.gmail.com')
-    MAIL_PORT =           int(os.getenv('MAIL_PORT',            '465'))
-    MAIL_USE_SSL =        int(os.getenv('MAIL_USE_SSL',         True))
+    USER_ENABLE_CONFIRM_EMAIL = False
+
 
     # Flask-User settings
     USER_APP_NAME        = "AppName"                # Used by email templates
